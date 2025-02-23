5926d3e3423bf0b351af9bab97588baeb71e1a2e
Nana Mohamadi <adilnana78@gmail.com>
2025-02-23 20:35:38 +0000

    Section 3 - User Auth. Form ( UI ) && Section 4 - Page Animation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
+import UseAuthForm from "./components/UseAuthForm";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Navbar />}>
-          <Route path="signin" element={<h1>Sign in page</h1>} />
-          <Route path="signup" element={<h1>Sign up page</h1>} />
+          <Route path="signin" element={<UseAuthForm type="sign-in"/>} />
+          <Route path="signup" element={<UseAuthForm type="sign-up"/>} />
         </Route>
       </Routes>
     </>
